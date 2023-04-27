611ee1b977d34562993e50ce5964eb055bb693eb
Fernando Suzin <suzin.dev@gmail.com>
2023-04-27 20:45:37 -0300

    Projeto criado

---
+++ src/test/java/com/suzintech/reactive/test/MonoTest.java
@@ -0,0 +1,13 @@
+package com.suzintech.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class MonoTest {
+
+    @Test
+    void test() {
+        log.info("Everything working as intended");
+    }
+}
